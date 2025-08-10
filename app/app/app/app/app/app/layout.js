2a7543907b8cf71ce88c395679b7b327d4ca271a
syvexx <gtaerotech@gmail.com>
2025-08-10 16:18:05 -0400

    Create layout.js

---
+++ app/app/app/app/app/app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "St. Matthew UCC" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  );
+}
